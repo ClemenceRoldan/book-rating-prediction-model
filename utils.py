577579e308bd5c005f903f099443adc351893231
Kheirie <kaderi.kheirie@gmail.com>
2024-02-25 01:40:21 +0100

    updates to data cleaning and featengineering

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 
+
 def to_lower(df: pd.DataFrame, columns: list):
     """funtionc to convert text columns to lower case
 
@@ -11,7 +12,8 @@ def to_lower(df: pd.DataFrame, columns: list):
     Returns:
         pd.DataFrame: the updated DataFrame
     """
-    df[columns] = df[columns].apply(lambda x: x.str.lower() if not pd.isna(x) else x)
+    for col in columns:
+        df[col] = df[col].apply(lambda x: x.lower() if not pd.isna(x) else x)
     return df
 
 def sub_text(df: pd.DataFrame, columns: list, replacements: list):
@@ -30,5 +32,25 @@ def sub_text(df: pd.DataFrame, columns: list, replacements: list):
         for col in columns:
             df[col] = df[col].apply(lambda x: re.sub(pattern, replacement, x) if not pd.isna(x) else x)
 
+    return df
+
+def one_hot_encode(df: pd.DataFrame, column_name: str):
+    """function to apply one hot encoding on categorical columns
+
+    Args:
+        df (pd.DataFrame): the dataframe
+        column_name (str): the column to apply one-hot encoding on
+
+    Returns:
+        pd.DataFrame: the updated DataFrame
+    """
+    encoded_columns = pd.get_dummies(df[column_name], prefix=column_name)
+    
+    # Concatenate the encoded columns to the original DataFrame
+    df = pd.concat([df, encoded_columns], axis=1)
+    
+    # Drop the original column
+    df.drop(column_name, axis=1, inplace=True)
+    
     return df
 
