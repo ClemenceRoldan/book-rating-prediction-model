07316948679528e7c2f5247f2ad14982b8e4b752
Kheirie <kaderi.kheirie@gmail.com>
2024-02-26 23:21:28 +0100

    updated utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,42 @@
 import pandas as pd
 import re
 
+def check_non_numerical_date(df: pd.DataFrame, columns_list: list, intended_type: str):
+    """function to check if specific columns have non_numerical or non_date values
 
+    Args:
+        df (pd.DataFrame): the dataframe
+        columns_list (list): list of columns to apply the function on
+        intended_type (str): the intended type to verify for (numerical or date)
+    """
+    
+    if intended_type == "numerical":
+        for column in columns_list:
+            non_numerical_values = df.loc[~df[column].apply(pd.to_numeric, errors='coerce').notna()]
+            print("The non_numerical_values in column {} : ".format(column), non_numerical_values[column])
+
+    elif intended_type == "date":
+        for column in columns_list:
+            non_numerical_values = df.loc[~df[column].apply(pd.to_datetime, errors='coerce').notna()]
+            print("The non_date_values in column {} : ".format(column), non_numerical_values[column])
+            
+# convert to numerical
+def convert_to_numerical(df: pd.DataFrame, columns_list: list):
+    """function to convert specific column into type numeric
+
+    Args:
+        df (pd.DataFrame): the dataframe
+        columns_list (list): list of columns to apply the function on
+
+    Returns:
+        pd.DataFrame: the updated DataFrame
+    """
+    
+    for column in columns_list:
+        df[column] = df[column].apply(pd.to_numeric)
+    
+    return df
+        
 def to_lower(df: pd.DataFrame, columns: list):
     """funtionc to convert text columns to lower case
 
